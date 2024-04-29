f2c492227db7eab6582c435ee4766ba6a3d7027c
sergiogrua <sergiogrua@ext.inditex.com>
2024-04-29 15:23:44 +0200

    fix sonar

--- src/main/java/sg/security/api/config/security/SecurityConfiguration.java
+++ src/main/java/sg/security/api/config/security/SecurityConfiguration.java
@@ -41,6 +41,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request.requestMatchers("/auths/**").permitAll()
                         .anyRequest()
                         .authenticated())
